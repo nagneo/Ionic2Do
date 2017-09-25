90a941f288c317a7224de80835836d9c181c181f
SAENAKIM <tosk3927@gmail.com>
2017-09-25 19:16:09 +0900

    Ionic2Do Hw is completed. Second Try

---
+++ src/pages/tasklist/tasklist.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { NavController, ItemSliding} from 'ionic-angular';
+import { AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+import { Task } from './task';
+
+@Component({
+  selector: 'page-tasklist',
+  templateUrl: 'tasklist.html'
+})
+export class TaskListPage {
+  tasks: FirebaseListObservable<any[]>;
+  constructor(public navCtrl: NavController, public af: AngularFireDatabase) {
+    this.tasks = af.list('/tasks');  
+  }
+
+  addItem()
+  {
+    let theNewTask: string = prompt("New Task");
+    if(theNewTask !== '')
+      {
+        this.tasks.push({title: theNewTask, status: 'open'});
+      }
+  }
+
+  markAsDone(slidingItem: ItemSliding, task: any){
+    this.tasks.update(task.$key, {status: "done"});
+    slidingItem.close();
+  }
+  removeTask(slidingItem: ItemSliding, task:any){
+    this.tasks.remove(task.$key);
+    slidingItem.close();
+  }
+}
+
